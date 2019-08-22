8e627838a93d8662888696d9a0d357c8988facec
Ahmad Helal <you@example.com>
2019-08-22 17:58:59 +0300

    solo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from "react";
 
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
+    loading: true,
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: this.authors,
+    authors: []
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  handleFitch = async () => {
+    try {
+      let response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      this.setState({ authors: response.data, loading: !this.state.loading });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  selectAuthor = async authorId => {
+    this.setState({ loading: true });
+    try {
+      let response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/" + authorId
+      );
+
+      this.setState({
+        currentAuthor: response.data,
+        loading: false
+      });
+      console.log(this.state.currentAuthor);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -30,20 +59,28 @@ class App extends Component {
   getContentView = () => {
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
-    } else {
+    } else if (!this.state.loading) {
       return (
         <AuthorsList
           authors={this.state.filteredAuthors}
           selectAuthor={this.selectAuthor}
           filterAuthors={this.filterAuthors}
+          authorsAPI={this.state.authors}
         />
       );
+    } else {
+      return <Loading />;
     }
   };
+
+  componentDidMount() {
+    this.handleFitch();
+  }
 
   render() {
     return (
       <div id="app" className="container-fluid">
+        {this.isLoading}
         <div className="row">
           <div className="col-2">
             <Sidebar unselectAuthor={this.unselectAuthor} />
